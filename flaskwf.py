2540d4cfbbfc3d280a63548f3011062c2d640c11
Ryan Martin <ryanmartinneutrino@gmail.com>
2016-10-16 16:46:25 +0000

    updated

--- flaskwf.py
+++ flaskwf.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from wifi import get_aps2, get_connection_info, write_wpa_file
+from wifi import get_aps2, get_connection_info, write_wpa_file, write_hostapd_conf
 
 app = Flask(__name__)
 
@@ -7,13 +7,25 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def wifilist():
+
+  if request.method == 'POST':
+  #The page was used to make a connection
+
+    if 'pwd' in request.form: 
+      #connect to a wifi
+      pwd=request.form['pwd']
+      ssid=request.form['ssid']
+      write_wpa_file(ssid, pwd)
+
+    if 'apssid' in request.form: 
+      pwd=request.form['appwd']
+      ssid=request.form['apssid']
+      ip=request.form['apip']      
+      write_hostapd_conf('wlan0', ssid, pwd, ip)
+
   aps = get_aps2('wlan0') 
   conn_info = get_connection_info('wlan0')
-  if request.method == 'POST':
-    pwd=request.form['pwd']
-    ssid=request.form['ssid']
-    write_wpa_file(ssid, pwd)
-
+ 
   return render_template("wifilist.html",aps = aps, conn_info = conn_info)
 
 
