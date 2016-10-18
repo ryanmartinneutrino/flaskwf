8904f1ea8c736f0fe3e9402545a91ca988d1fdc0
Ryan Martin <ryanmartinneutrino@gmail.com>
2016-10-18 22:53:45 +0000

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#sudo apt-get update
+sudo apt-get install iw hostapd
+
+#make backups
+sudo cp /etc/network/interfaces /etc/network/interfaces.orig
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.orig
+sudo cp 
+
+
+
