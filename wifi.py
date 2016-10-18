647afffc2f2646943478ba18bb0b3bcf62d5cb94
Ryan Martin <ryanmartinneutrino@gmail.com>
2016-10-18 22:53:19 +0000

    update

--- wifi.py
+++ wifi.py
@@ -6,6 +6,9 @@ import os
 
 def connect_wifi(ssid, pwd, iface='wlan0'):
   write_wpa_conf(ssid, pwd)
+  write_network_interfaces_WF(iface)
+  sp.call('sudo cp /etc/network/interfaces /etc/network/interfaces.bak',shell=True)  
+  sp.call('sudo cp interfaces.wifi /etc/network/interfaces',shell=True)  
   sp.call('sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak', shell=True)
   sp.call('sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf',shell=True)
   sp.call('sudo ifdown '+iface, shell=True)
@@ -16,8 +19,15 @@ def start_ap(ssid = 'flaskwf', pwd = '1257Berkeley', ip = '10.10.0.1', iface='wl
 
   write_hostapd_conf(iface, ssid, pwd, ip)
   write_network_interfaces_AP(ip, iface)
+  write_dchpd_conf(network='10.10.0.0', ip=ip)
   sp.call('sudo cp hostapd.conf /etc/hostapd/hostapd.conf',shell=True)  
-  sp.call('sudo cp interfaces /etc/network/interfaces',shell=True)  
+  sp.call('sudo cp interfaces.ap /etc/network/interfaces',shell=True)  
+  sp.call('sudo cp dhcpd.conf /etc/dhcp/dhcpd.conf',shell=True)  
+  sp.call('sudo ifdown '+iface, shell=True)
+  sp.call('sudo ifup '+iface, shell=True)
+
+  sp.call('sudo service hostapd restart', shell=True)
+  #sp.call('sudo  ', shell=True)
 
 
 def write_wpa_conf(ssid, pwd):
@@ -47,26 +57,57 @@ rsn_pairwise=CCMP\n\
   hfile.write(info)
   hfile.close()
 
+
+
+def write_network_interfaces_WF(iface='wlan0' ):
+  info = "\
+auto lo\n\
+iface lo inet loopback \n\
+iface eth0 inet dhcp\n\
+\n\
+auto {iface}\n\
+allow-hotplug {iface}\n\
+iface {iface} inet dhcp\n\
+        wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf\n\
+".format(iface=iface)
+
+  hfile = open("interfaces.wifi","w")
+  hfile.write(info)
+  hfile.close()
+
 def write_network_interfaces_AP(ip = '10.10.0.1', iface='wlan0' ):
   '''Write the /etc/network/interfaces file for AP mode'''
   network = '10.10.0.0'
   info = "\
 auto lo\n\
-iface lo inet loopbac \n\
+iface lo inet loopback\n\
 iface eth0 inet dhcp\n\
 \n\
-allow-hotplug {}\n\
-iface {} inet static\n\
-	address {}\n\
+#auto {iface}\n\
+allow-hotplug {iface}\n\
+iface {iface} inet static\n\
+	address {ip}\n\
 	netmask 255.255.255.0\n\
-	network {}\n\
-	broadcast 10.0.0.255\n\
-       	wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf\n\
-".format(iface,iface, ip, network)
+".format(iface=iface, ip=ip)
 
-  hfile = open("interfaces", "w")
+  hfile = open("interfaces.ap", "w")
   hfile.write(info)
   hfile.close()
+
+def write_dhcpd_conf(network='10.10.0.0', ip = '10.10.0.1'):
+  info = "\
+ddns-update-style none;\n\
+default-lease-time 84600;\n\
+max-lease-time 84600;\n\
+subnet {network} netmask 255.255.255.0 {{\n\
+  range 10.10.0.2 10.10.0.100  ;\n\
+  option domain-name-servers 8.8.8.8 ;\n\
+  option routers  {ip} ;\n\
+}}\n\
+".format(network=network, ip=ip)
+  file = open("dhcpd.conf", "w")
+  file.write(info)
+  file.close()
 
 
 
